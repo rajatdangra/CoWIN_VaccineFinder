382bc1f4ca808fc4f6f8b9dc0fb223d90e676f97
Rajat Maheshwari <rajat.dangra@gmail.com>
2021-06-09 04:07:48 +0530

    VaccineSlotBook: Added Datetime extension methods.

--- VaccineFinder/Methods/ExtensionMethods/DateTimeExtensions.cs
+++ VaccineFinder/Methods/ExtensionMethods/DateTimeExtensions.cs
@@ -8,8 +8,13 @@ namespace VaccineFinder
 {
     public static class DateTimeExtensions
     {
-        public static bool IsInPeriod(this DateTime date, DateTime startDate, DateTime endDate)
+        public static bool IsInPeriod(this DateTime date, DateTime startDate, DateTime endDate, bool compareStartAndEOD = true)
         {
+            if (compareStartAndEOD)
+            {
+                startDate = startDate.StartOfDay();
+                endDate = endDate.EndOfDay();
+            }
             return date >= startDate && date <= endDate;
         }
 
@@ -17,10 +22,26 @@ namespace VaccineFinder
         {
             return date == default(DateTime);
         }
-
         public static string ToDetailString(this DateTime date)
         {
             return date.ToString("dd/MMM/yyyy hh:mm:ss tt");
+        }
+
+        public static DateTime StartOfDay(this DateTime dateTime)
+        {
+            return new DateTime(
+             dateTime.Year,
+             dateTime.Month,
+             dateTime.Day,
+             0, 0, 0, 0);
+        }
+        public static DateTime EndOfDay(this DateTime dateTime)
+        {
+            return new DateTime(
+             dateTime.Year,
+             dateTime.Month,
+             dateTime.Day,
+             23, 59, 59, 999);
         }
     }
 }
